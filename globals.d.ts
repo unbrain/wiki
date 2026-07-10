d28b6c27b41eac206583d944ad0871b618eeb97d
unbrain <marsorsun@gmail.com>
2026-07-11 00:45:33 +0800

    vault backup: 2026-07-11 00:45:33

--- globals.d.ts
+++ globals.d.ts
@@ -1,17 +1,8 @@
-export declare global {
-  interface Document {
-    addEventListener<K extends keyof CustomEventMap>(
-      type: K,
-      listener: (this: Document, ev: CustomEventMap[K]) => void,
-    ): void
-    removeEventListener<K extends keyof CustomEventMap>(
-      type: K,
-      listener: (this: Document, ev: CustomEventMap[K]) => void,
-    ): void
-    dispatchEvent<K extends keyof CustomEventMap>(ev: CustomEventMap[K] | UIEvent): void
-  }
-  interface Window {
-    spaNavigate(url: URL, isBack: boolean = false)
-    addCleanup(fn: (...args: any[]) => void)
-  }
+declare module "micromorph" {
+  function micromorph(
+    old: Element | string,
+    new_: Element | string,
+    options?: { useMicrodata?: boolean }
+  ): void;
+  export default micromorph;
 }
